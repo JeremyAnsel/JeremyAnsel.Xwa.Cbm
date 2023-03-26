c1247b0fa997f8f333a6a753e9b6f5ddaa2bda77
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2023-03-26 13:48:50 +0200

    Add stream methods

--- JeremyAnsel.Xwa.Cbm/JeremyAnsel.Xwa.Cbm/CbmFile.cs
+++ JeremyAnsel.Xwa.Cbm/JeremyAnsel.Xwa.Cbm/CbmFile.cs
@@ -69,83 +69,73 @@ namespace JeremyAnsel.Xwa.Cbm
             }
         }
 
-        [SuppressMessage("Style", "IDE0017:Simplifier l'initialisation des objets", Justification = "Reviewed.")]
         public static CbmFile FromFile(string fileName)
         {
-            var cbm = new CbmFile
+            using var filestream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+
+            CbmFile cbm = FromStream(filestream);
+            cbm.FileName = fileName;
+
+            return cbm;
+        }
+
+        [SuppressMessage("Style", "IDE0017:Simplifier l'initialisation des objets", Justification = "Reviewed.")]
+        public static CbmFile FromStream(Stream stream)
+        {
+            var cbm = new CbmFile();
+
+            using var file = new BinaryReader(stream, Encoding.ASCII, true);
+
+            int count = file.ReadInt32();
+
+            cbm.currentImageIndex = file.ReadInt32();
+
+            file.BaseStream.Position += 16;
+
+            cbm.ImageId = file.ReadInt32();
+            cbm.GroupId = file.ReadInt32();
+
+            file.BaseStream.Position += 4;
+
+            if (cbm.currentImageIndex < 0 || cbm.currentImageIndex >= count)
             {
-                FileName = fileName
-            };
-
-            FileStream filestream = null;
-
-            try
-            {
-                filestream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-
-                using (BinaryReader file = new BinaryReader(filestream, Encoding.ASCII))
-                {
-                    filestream = null;
-
-                    int count = file.ReadInt32();
-
-                    cbm.currentImageIndex = file.ReadInt32();
-
-                    file.BaseStream.Position += 16;
-
-                    cbm.ImageId = file.ReadInt32();
-                    cbm.GroupId = file.ReadInt32();
-
-                    file.BaseStream.Position += 4;
-
-                    if (cbm.currentImageIndex < 0 || cbm.currentImageIndex >= count)
-                    {
-                        cbm.currentImageIndex = 0;
-                    }
-
-                    for (int i = 0; i < count; i++)
-                    {
-                        var image = new CbmImage();
-
-                        image.Width = file.ReadInt32();
-                        image.Height = file.ReadInt32();
-                        image.IsCompressed = file.ReadInt32() != 0;
-
-                        int dataLength = file.ReadInt32();
-
-                        image.AreaLeft = file.ReadInt32();
-                        image.AreaTop = file.ReadInt32();
-                        image.AreaRight = file.ReadInt32();
-                        image.AreaBottom = file.ReadInt32();
-
-                        file.BaseStream.Position += 4;
-
-                        image.palette16 = new ushort[256];
-
-                        for (int c = 0; c < 256; c++)
-                        {
-                            image.palette16[c] = (ushort)file.ReadUInt32();
-                        }
-
-                        image.palette32 = new uint[256];
-
-                        for (int c = 0; c < 256; c++)
-                        {
-                            image.palette32[c] = file.ReadUInt32();
-                        }
-
-                        image.rawData = file.ReadBytes(dataLength);
-
-                        cbm.Images.Add(image);
-                    }
-                }
+                cbm.currentImageIndex = 0;
             }
-            finally
+
+            for (int i = 0; i < count; i++)
             {
-                if (filestream != null)
+                var image = new CbmImage();
+
+                image.Width = file.ReadInt32();
+                image.Height = file.ReadInt32();
+                image.IsCompressed = file.ReadInt32() != 0;
+
+                int dataLength = file.ReadInt32();
+
+                image.AreaLeft = file.ReadInt32();
+                image.AreaTop = file.ReadInt32();
+                image.AreaRight = file.ReadInt32();
+                image.AreaBottom = file.ReadInt32();
+
+                file.BaseStream.Position += 4;
+
+                image.palette16 = new ushort[256];
+
+                for (int c = 0; c < 256; c++)
                 {
-                    filestream.Dispose();
+                    image.palette16[c] = (ushort)file.ReadUInt32();
                 }
+
+                image.palette32 = new uint[256];
+
+                for (int c = 0; c < 256; c++)
+                {
+                    image.palette32[c] = file.ReadUInt32();
+                }
+
+                image.rawData = file.ReadBytes(dataLength);
+
+                cbm.Images.Add(image);
             }
 
             return cbm;
@@ -153,62 +143,51 @@ namespace JeremyAnsel.Xwa.Cbm
 
         public void Save(string fileName)
         {
-            FileStream filestream = null;
+            using var filestream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
 
-            try
+            this.Save(filestream);
+            this.FileName = fileName;
+        }
+
+        public void Save(Stream stream)
+        {
+            using var file = new BinaryWriter(stream, Encoding.ASCII, true);
+
+            file.Write(this.Images.Count);
+            file.Write(this.currentImageIndex);
+            file.Write(this.AreaLeft);
+            file.Write(this.AreaTop);
+            file.Write(this.AreaRight);
+            file.Write(this.AreaBottom);
+            file.Write(this.ImageId);
+            file.Write(this.GroupId);
+            file.Write(0);
+
+            foreach (var image in this.Images)
             {
-                filestream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+                file.Write(image.Width);
+                file.Write(image.Height);
+                file.Write(image.IsCompressed ? 1 : 0);
+                file.Write(image.rawData == null ? 0 : image.rawData.Length);
+                file.Write(image.AreaLeft);
+                file.Write(image.AreaTop);
+                file.Write(image.AreaRight);
+                file.Write(image.AreaBottom);
+                file.Write(0);
 
-                using (BinaryWriter file = new BinaryWriter(filestream, Encoding.ASCII))
+                for (int c = 0; c < 256; c++)
                 {
-                    filestream = null;
-
-                    file.Write(this.Images.Count);
-                    file.Write(this.currentImageIndex);
-                    file.Write(this.AreaLeft);
-                    file.Write(this.AreaTop);
-                    file.Write(this.AreaRight);
-                    file.Write(this.AreaBottom);
-                    file.Write(this.ImageId);
-                    file.Write(this.GroupId);
-                    file.Write(0);
-
-                    foreach (var image in this.Images)
-                    {
-                        file.Write(image.Width);
-                        file.Write(image.Height);
-                        file.Write(image.IsCompressed ? 1 : 0);
-                        file.Write(image.rawData == null ? 0 : image.rawData.Length);
-                        file.Write(image.AreaLeft);
-                        file.Write(image.AreaTop);
-                        file.Write(image.AreaRight);
-                        file.Write(image.AreaBottom);
-                        file.Write(0);
-
-                        for (int c = 0; c < 256; c++)
-                        {
-                            file.Write((uint)image.palette16[c]);
-                        }
-
-                        for (int c = 0; c < 256; c++)
-                        {
-                            file.Write(image.palette32[c]);
-                        }
-
-                        if (image.rawData != null)
-                        {
-                            file.Write(image.rawData);
-                        }
-                    }
-
-                    this.FileName = fileName;
+                    file.Write((uint)image.palette16[c]);
                 }
-            }
-            finally
-            {
-                if (filestream != null)
+
+                for (int c = 0; c < 256; c++)
                 {
-                    filestream.Dispose();
+                    file.Write(image.palette32[c]);
+                }
+
+                if (image.rawData != null)
+                {
+                    file.Write(image.rawData);
                 }
             }
         }
